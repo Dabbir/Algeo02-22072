72c3e8bde40412844f4c4d009be87edaaa33ad27
William Glory <williamgloryh@gmail.com>
2023-11-18 18:55:35 +0700

    feat : about and description

---
+++ algeo02/src/app/description/layout.js
@@ -0,0 +1,20 @@
+// src/components/Layout.js
+
+import React from "react";
+import "./description.css";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <header>
+        {/* Add navigation if needed */}
+      </header>
+      <main>{children}</main>
+      <footer>
+        {/* Add footer content if needed */}
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
